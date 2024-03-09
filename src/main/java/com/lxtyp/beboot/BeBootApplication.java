039e402a42e51418cbb2b9eabe63ad37b3e52e72
taoyp <taoyipingc@gmail.com>
2024-03-10 07:55:46 +0800

    first commit
    first commit

---
+++ src/main/java/com/lxtyp/beboot/BeBootApplication.java
@@ -0,0 +1,13 @@
+package com.lxtyp.beboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lxtyp/beboot/BeBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxtyp.beboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
